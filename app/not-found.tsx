ea7174ca8abc5692ecb989d57ec1e8e49eaf911d
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-07-10 16:18:25 -0400

    create 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import Link from 'next/link'
+
+export default function NotFound() {
+    return (
+        <main className="min-h-screen bg-background-hue font-quicksand text-text-hue">
+            <Header />
+            <div className="mt-48 flex flex-col justify-center items-center px-12 text-center border-0 border-red-300">
+                <h1 className="break-words text-4xl font-mono mb-8">
+                    404 - Page Not Found
+                </h1>
+                <p>
+                    Looking for something else? Go <Link href="/" className="text-accent-hue underline">home</Link>.
+                </p>
+            </div>
+            <Footer/>
+        </main>
+    )
+}
