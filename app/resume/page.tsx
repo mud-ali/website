0410609f9f6442c046e6fbcd87ac1832669fde00
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-07-11 11:53:09 -0400

    stylistic rework of resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -10,7 +10,8 @@ export default function Resume() {
             <div className="content px-6 sm:mt-16 h-screen">
                 <iframe
                     src="/Resume_1.pdf"
-                    className="w-full h-full"
+                    className={`w-2/3 h-full mx-auto rounded-2xl shadow-[0px_0px_5px_1px_rgba(185,185,177,1)]
+                                hover:shadow-[0px_0px_10px_5px_rgba(195,185,177,1)] duration-500`}
                 >
                 </iframe>
             </div>
