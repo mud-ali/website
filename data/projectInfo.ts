b72c265bd93ef5d5ceed356d66c73d02701bbc40
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-07-11 15:39:53 -0400

    edit project details

--- data/projectInfo.ts
+++ data/projectInfo.ts
@@ -16,9 +16,9 @@ import Rust from "@/assets/rust.svg"
 export const projectInfo : Project[] = [
     {
         name: "BCACTF 5.0",
-        desc: "Wrote CTF (capture the flag) challenges for BCACTF 5.0, my high school CTF club's annual cybersecurity competition. As president of the club, I also managed and reviewed other challenge authors' problems to ensure that they were solvable and fun. In addition, I worked with other administrators to secure sponsorships, deploy infrastructure, and respond to support tickets in a timely manner. <br /> <br /> Special thanks to Google Cloud (https://cloud.google.com/ ), Binary Ninja (https://binary.ninja/ ), TryHackMe (https://tryhackme.com/ ), and the New Jersey Cybersecurity and Communications Integration Cell (https://www.cyber.nj.gov/home-njccic ) for sponsoring this event.",
+        desc: "Wrote CTF (capture the flag) challenges for BCACTF 5.0, my high school CTF club's annual cybersecurity competition. As president of the club, I also managed and reviewed other challenge authors' problems to ensure that they were solvable and fun. In addition, I worked with other administrators to secure sponsorships, deploy infrastructure, and respond to support tickets in a timely manner.",
         icon: BCACTFV,
-        when: "June 7th - June 10th, 2024",
+        when: "06/07/2024 - 06/10/2024",
         url: ["https://github.com/BCACTF/bcactf-5.0", "https://5.bcactf.com/"],
         tech: [Docker, Git, Python, Rust],
         techTitle: ["Docker", "Git", "Python", "Java", "Rust"]
@@ -27,16 +27,16 @@ export const projectInfo : Project[] = [
         name: "First Tech Challenge - Centerstage",
         desc: "As a member of the Knightmare Robotics (15891) team, I co-led the programming subteam for the 2023-2024 Centerstage<sup>®</sup> challenge. I independently designed and tuned a computer vision algorithm using OpenCV to detect the location of a landmark object, affecting the autonomous path chosen by the robot. In addition, I worked with other programmers to tune the PID control system used by the drivetrain for accurate autonomous movement. I also delivered lessons on PID using the Road Runner library and about the basics of the FTC SDK.",
         icon: Titanium,
-        when: "September 9th, 2023 - February 25th, 2024",
+        when: "09/09/2023 - 02/25/2024",
         url: ["https://github.com/titanium-knights/knightmares-centerstage-2023", "https://github.com/titanium-knights/knightmares-centerstage-2024"],
         tech: [Java, Kotlin, Roadrunner, OpenCV],
         techTitle: ["Java", "Kotlin", "Roadrunner", "OpenCV"]
     },
     {
-        name: "Website",
+        name: "Personal Website",
         desc: "Designed personal website using NextJS and TailwindCSS. For more information, check out the source code on github: https://github.com/mud-ali/website .",
         icon: GithubLogo,
-        when: "July 7th, 2024 - Present",
+        when: "07/07/2024 - Present",
         tech: [NextJS, Tailwind],
         techTitle: ["NextJS", "TailwindCSS"]
     },
