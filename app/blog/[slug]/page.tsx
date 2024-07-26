f632cc3e4de4beb5e0295ace4474d11422b9d6e7
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-07-25 21:57:35 -0400

    add individual page for each post

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import { Header } from "@/components/Header"
+import { Footer } from "@/components/Footer"
+import { ReactNode } from "react"
+import { blogPostsList } from "@/data/blogPostsList"
+
+export default function Blog({ params }: { params: { slug: string } }): ReactNode {
+    for (let i=0;i<blogPostsList.length;i++) {
+        if (blogPostsList[i].url === `/blog/${params.slug}`) {
+            return (
+                <main className="min-h-screen h-full bg-background-hue font-head text-text-hue">
+                    <Header />
+                    <div id="top" className="text-center my-24">
+                        <h1 className="text-3xl mb-4 font-extrabold font-body text-white">
+                            {blogPostsList[i].title}
+                        </h1>
+                        <p>
+                            {blogPostsList[i].description}
+                        </p>
+                    </div>
+                    <Footer />
+                </main>
+            )
+        }
+    }
+    return (
+        <main className="min-h-screen h-full bg-background-hue font-head text-text-hue">
+            <Header />
+            <div id="top" className="text-center my-24">
+                <h1 className="text-3xl mb-4 font-extrabold font-body text-white">
+                    404
+                </h1>
+                <p>
+                    This post does not exist. Check the URL or go back <a href="/blog" className="text-accent-hue underline">home</a>.
+                </p>
+            </div>
+            <Footer />
+        </main>
+    )
+}
