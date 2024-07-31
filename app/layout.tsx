9fea4fad27e83339ded268f2d34b274c09fb0c50
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-07-30 21:38:30 -0400

    improve seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,23 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Mudasir Ali',
-  description: 'Student. Engineer. Programmer.',
+    title: 'Mudasir Ali',
+    description: 'Student. Engineer. Programmer.',
+    keywords: [
+        'Mudasir', 'Mudasir Ali', 'Engineer', 'Programmer', 'Cybersecurity', 
+        'Robotics', 'Web Development', 'Software Development', 
+        'Bergen County Academies', 'BCA'
+    ],
+    creator: 'Mudasir Ali',
+    robots: { index: true, follow: false },
+    icons: {
+        icon: "/favicon.ico",
+        apple: "/favicon.ico",
+    },
+    twitter: {
+        card: "summary_large_image",
+        images: "/favicon.ico",
+    },
 }
 
 export default function RootLayout({
