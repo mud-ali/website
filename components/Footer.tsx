ca66ecc58b75562998b2ebaf7eb3b62cc1571985
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-01-02 20:31:17 -0500

    increase copyright year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <div id="footer" className={`pb-8 text-center w-full sticky top-[98vh] bottom-2`}>
-            &copy; 2024 Mudasir Ali. Made <a
+            &copy; 2025 Mudasir Ali. Made <a
                 href="https://www.github.com/mud-ali/website"
                 className="text-accent-hue underline"
             >with ❤️</a>.
